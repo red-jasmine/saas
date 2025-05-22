c17ee5e94624fe395631117bd644576c9b294352
liushoukun <liushoukun66@gmail.com>
2025-05-22 18:32:42 +0800

    refactor(admin): 重构管理员面板并集成 Filament Admin
    -继承 RedJasmine\FilamentAdmin\AdminPanelProvider 以简化代码结构
    - 移除冗余的中间件配置和自定义设置
    - 删除未使用的 Filament Shield 相关代码
    - 更新 composer.json 和 composer.lock 文件

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,26 +3,9 @@
 namespace App\Providers\Filament;
 
 
-use BezhanSalleh\FilamentShield\Facades\FilamentShield;
-use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\AuthenticateSession;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages;
 use Filament\Panel;
-use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Support\Enums\MaxWidth;
-use Filament\Widgets;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Session\Middleware\StartSession;
-use Illuminate\Support\Str;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
-use RedJasmine\FilamentAdmin\FilamentAdminPlugin;
 use RedJasmine\FilamentArticle\FilamentArticlePlugin;
 use RedJasmine\FilamentCard\FilamentCardPlugin;
 use RedJasmine\FilamentCommunity\FilamentCommunityPlugin;
@@ -33,76 +16,35 @@ use Stancl\Tenancy\Middleware\InitializeTenancyByDomainOrSubdomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
 
-class AdminPanelProvider extends PanelProvider
+class AdminPanelProvider extends \RedJasmine\FilamentAdmin\AdminPanelProvider
 {
 
 
-    public function boot() : void
-    {
-        FilamentShield::configurePermissionIdentifierUsing(function ($resource) {
-
-            return Str::of($resource::getModel())
-                      ->toString();
-
-        });
-
-    }
-
     public function panel(Panel $panel) : Panel
     {
+        $panel = parent::panel($panel);
         return $panel
-            ->default()
-            ->id('admin')
-            ->path('admin')
-            ->login(Login::class)
             ->colors([
                 'primary' => Color::Amber,
             ])
             ->maxContentWidth(MaxWidth::Full)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
-            ->pages([
-                Pages\Dashboard::class,
-            ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
-            ])
-            ->middleware([
-                EncryptCookies::class,
-                AddQueuedCookiesToResponse::class,
-                StartSession::class,
-                AuthenticateSession::class,
-                ShareErrorsFromSession::class,
-                VerifyCsrfToken::class,
-                SubstituteBindings::class,
-                DisableBladeIconComponents::class,
-                DispatchServingFilamentEvent::class,
-            ])
             ->middleware([
                 'universal',
                 InitializeTenancyByDomainOrSubdomain::class,
                 //PreventAccessFromCentralDomains::class,
             ], isPersistent: true)
-            ->authMiddleware([
-                Authenticate::class,
-            ])
             ->sidebarWidth('10rem')
-            ->authGuard('admin')
-            //->registration()
-            ->passwordReset()
-            ->emailVerification()
-            ->profile()
             ->plugins([
-                FilamentShieldPlugin::make(),
                 FilamentUserPlugin::make(),
                 FilamentProductPlugin::make(),
                 FilamentOrderPlugin::make(),
                 FilamentCardPlugin::make(),
                 FilamentCommunityPlugin::make(),
                 FilamentArticlePlugin::make(),
-                FilamentAdminPlugin::make(),
+
             ]);
     }
 }
