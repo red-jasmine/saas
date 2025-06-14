599cd2b3904b8f672393dfc070b505b672e9d633
liushoukun <liushoukun66@gmail.com>
2025-06-14 16:00:48 +0800

    fix(config): 设置中央域以解决租户识别问题
    - 将 'central_domains' 配置修改为包含本地测试域的数组- 添加了 '127.0.0.1', 'saas.localhost' 和 'saas.test' 作为中央域
    - 此修改有助于在本地开发环境中正确识别租户

--- config/tenancy.php
+++ config/tenancy.php
@@ -16,7 +16,11 @@ return [
      *
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
-    'central_domains'      => env('central_domains'),
+    'central_domains'      => [
+        '127.0.0.1',
+        'saas.localhost',
+        'saas.test',
+    ],
 
     /**
      * Tenancy bootstrappers are executed when tenancy is initialized.
