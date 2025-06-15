2db868bdfe6ff3fe4f56a39f6edb23f27bdfbbbb
liushoukun <liushoukun66@gmail.com>
2025-06-15 17:30:03 +0800

    feat(config): 添加阿里云 OSS 文件系统支持
    - 在 filesystems.php 中添加了 oss 配置项，用于连接阿里云 OSS 存储
    - 在 composer.json 中添加了 laravel-filesystem-oss依赖
    - 更新 composer.lock 文件，包含阿里云 OSS SDK 和相关依赖

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,28 @@ return [
             'report' => false,
         ],
 
+
+        'oss' => [
+            'driver' => 'oss',
+            'root' => '', // 设置上传时根前缀
+            'access_key' => env('OSS_ACCESS_KEY'), //
+            'secret_key' => env('OSS_SECRET_KEY'), //
+            'endpoint'   => env('OSS_ENDPOINT'), // 使用 ssl 这里设置如: https://oss-cn-shenzhen.aliyuncs.com
+            'bucket'     => env('OSS_BUCKET'),
+            'isCName'    => env('OSS_IS_CNAME', false), // 如果 isCname 为 false，endpoint 应配置 oss 提供的域名如：`oss-cn-beijing.aliyuncs.com`，否则为自定义域名，，cname 或 cdn 请自行到阿里 oss 后台配置并绑定 bucket
+            // 额外自定义初始化 OSS 客户端参数
+            // 参考:
+            // 1. https://help.aliyun.com/zh/oss/developer-reference/initialization-6
+            // 2. \OSS\OssClient::__initNewClient
+            // 3. \OSS\OssClient::__initClient
+            'signatureVersion' => env('OSS_SIGNATURE_VERSION','V4'),
+            'region'           => env('OSS_REGION', 'cn-shenzhen'),
+            // 如果有更多的 bucket 需要切换，就添加所有bucket，默认的 bucket 填写到上面，不要加到 buckets 中
+            'buckets'=>[
+
+            ],
+        ],
+
     ],
 
     /*
