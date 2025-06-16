51c9417f845fa95506f009798bbbc632a507f6bd
liushoukun <liushoukun66@gmail.com>
2025-06-16 10:36:13 +0800

    fix:优化 404 错误处理
    - 添加自定义的 404 错误响应，针对 API 请求返回 JSON 格式
    - 通过判断请求路径，为 /api/* 开头的请求提供特定的错误处理
    - 保持原有的路由、中间件和异常处理结构不变

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,23 +6,34 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
-        $middleware->group('universal',[]);
-        $middleware->web(append: [
-            HandleAppearance::class,
-            HandleInertiaRequests::class,
-            AddLinkHeadersForPreloadedAssets::class,
-        ]);
+                  ->withRouting(
+                      web: __DIR__.'/../routes/web.php',
+                      commands: __DIR__.'/../routes/console.php',
+                      health: '/up',
+                  )
+                  ->withMiddleware(function (Middleware $middleware) {
+                      $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+                      $middleware->group('universal', []);
+                      $middleware->web(append: [
+                          HandleAppearance::class,
+                          HandleInertiaRequests::class,
+                          AddLinkHeadersForPreloadedAssets::class,
+                      ]);
 
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+                  })
+                  ->withExceptions(function (Exceptions $exceptions) {
+
+                      $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+                          if($request->is('api/*')){
+                              return response()->json(['error' => 404, 'message' => 'Record not found.'], 404);
+
+                          }
+
+                      });
+                      //dd($exceptions);
+                      //
+                  })->create();
