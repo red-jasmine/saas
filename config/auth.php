3fe21d5dc3d08509a105a5f4ec1d00ff0ee73bfd
liushoukun <liushoukun66@gmail.com>
2025-06-30 15:35:35 +0800

    ci(deps): 更新 PHP 版本并添加用户认证配置
    - 将 PHP 版本要求从 ^8.3 提升到 ^8.4
    - 在 auth.php 文件中添加了 'user'配置，使用 JWT 驱动和用户提供者

--- config/auth.php
+++ config/auth.php
@@ -44,6 +44,10 @@ return [
             'driver' => 'jwt',
             'provider' => 'users',
         ],
+        'user' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+        ],
         'admin' => [
             'driver' => 'session',
             'provider' => 'admins',
