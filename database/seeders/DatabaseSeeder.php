bc20b4d15f654e3622aa922e1d02c92a6311ae95
liushoukun <liushoukun66@gmail.com>
2026-01-08 18:13:47 +0800

    ``` feat(database): 更新数据库填充器以创建管理员用户
    - 添加了对 RedJasmine\Admin\Domain\Models\Admin 模型的引用
    - 修改了填充逻辑以创建管理员用户而非测试用户
    - 设置管理员默认凭据 (用户名: admin, 邮箱: admin@red-jasmine.top, 密码: admin)
    - 调整了代码格式，包括方法签名中的空格
    ```

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,24 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use RedJasmine\Admin\Domain\Models\Admin;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run() : void
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Admin::factory()->create([
+            'name'     => 'admin',
+            'email'    => 'admin@red-jasmine.top',
+            'password' => 'admin'
         ]);
     }
 }
